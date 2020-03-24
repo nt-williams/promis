d90fe0145304a5f4ab06e12171b935805d98b9b1
Nick Williams <ntwilliams.personal@gmail.com>
2020-03-24 16:40:10 -0400

    importing entire vctrs package

---
+++ R/promis-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' Internal vctrs methods
+#'
+#' @import vctrs
+#' @keywords internal
+#' @name pizza-vctrs
+NULL
