239dd2989cea98731116d63f21798f3b46470196
Nick Williams <ntwilliams.personal@gmail.com>
2020-03-25 17:32:15 -0400

    base promis cutoff functions created

--- R/promis_cutoffs.R
+++ R/promis_cutoffs.R
@@ -25,50 +25,108 @@ promis_adult_profile_symptoms <- function(x) {
   }, simplify = TRUE)
 }
 
-promis_pediatric_profile <- function(x) {
+promis_ped_parent_profile <- function(x) {
   sapply(x, function(x) {
-    # TODO
+    if (x <= 50)
+      "Normal limits"
+    else if (x <= 55)
+      "Mild"
+    else if (x <= 65)
+      "Moderate"
+    else if (x > 65)
+      "Severe"
   }, simplify = TRUE)
 }
 
-promis_pediatric_profile_sleep <- function(x) {
+promis_ped_parent_profile_sleep <- function(x) {
   sapply(x, function(x) {
-    # TODO
+    if (x <= 55)
+      "Normal limits"
+    else if (x <= 59)
+      "Mild"
+    else if (x <= 65)
+      "Moderate"
+    else if (x > 65)
+      "Severe"
   }, simplify = TRUE)
 }
 
-promis_pediatric_profile_relation <- function(x) {
+promis_ped_parent_profile_relation <- function(x) {
   sapply(x, function(x) {
-    # TODO
+    if (x >= 60)
+      "Excellent"
+    else if (x >= 40)
+      "Good"
+    else if (x >= 30)
+      "Fair"
+    else if (x < 30)
+      "Poor"
   }, simplify = TRUE)
 }
 
-promis_vl_vh_one <- function(x) {
+promis_vl_vh <- function(x) {
   sapply(x, function(x) {
-    # TODO
-  }, simplify = TRUE)
-}
-
-promis_vl_vh_two <- function(x) {
-  sapply(x, function(x) {
-    # TODO
+    if (x <= 30)
+      "Very low"
+    else if (x <= 40)
+      "Low"
+    else if (x <= 60)
+      "Average"
+    else if (x <= 70)
+      "High"
+    else if (x > 70)
+      "Very high"
   }, simplify = TRUE)
 }
 
 promis_adult_global_health <- function(x) {
   sapply(x, function(x) {
-    # TODO
+    if (x >= 58)
+      "Excellent"
+    else if (x >= 50)
+      "Very good"
+    else if (x >= 42)
+      "Good"
+    else if (x >= 35)
+      "Fair"
+    else if (x < 35)
+      "Poor"
   }, simplify = TRUE)
 }
 
 promis_adult_global_mental <- function(x) {
   sapply(x, function(x) {
-    # TODO
+    if (x >= 56)
+      "Excellent"
+    else if (x >= 48)
+      "Very good"
+    else if (x >= 40)
+      "Good"
+    else if (x >= 29)
+      "Fair"
+    else if (x < 29)
+      "Poor"
   }, simplify = TRUE)
 }
 
-promis_pediatric_global_health <- function(x) {
+promis_ped_global_health <- function(x) {
   sapply(x, function(x) {
-    # TODO
+    if (x >= 42)
+      "Good"
+    else if (x >= 37)
+      "Fair"
+    else if (x < 36)
+      "Poor"
+  }, simplify = TRUE)
+}
+
+promis_parent_global_health <- function(x) {
+  sapply(x, function(x) {
+    if (x >= 45)
+      "Good"
+    else if (x >= 36)
+      "Fair"
+    else if (x < 36)
+      "Poor"
   }, simplify = TRUE)
 }
